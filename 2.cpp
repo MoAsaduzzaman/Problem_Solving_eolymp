aea34c6779938ee29fbff1ecd003f74093aca7f2
MoAsadduzzaman <asaduzzamanpersonal123@gmail.com>
2023-10-28 00:05:46 +0600

    Digis

---
+++ 2.cpp
@@ -0,0 +1,13 @@
+//Problem name: 2 
+//https://www.eolymp.com/en/problems/2
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string n;
+    cin >> n;
+
+    cout << n.length() << endl;
+
+    return 0;
+}
